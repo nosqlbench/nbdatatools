a9cb5b14218ebeadf011836cf1da6184527351ad
Jonathan Shook <jshook@gmail.com>
2025-04-16 02:03:24 -0500

    MerkleMeta additions

--- vectordata/src/main/java/io/nosqlbench/vectordata/download/merkle/MerkleFooter.java
+++ vectordata/src/main/java/io/nosqlbench/vectordata/download/merkle/MerkleFooter.java
@@ -1,4 +1,7 @@
 package io.nosqlbench.vectordata.download.merkle;
 
+/// a record type for the footer of a merkle tree file
+/// @param chunkSize the chunk size in bytes
+/// @param totalSize the total size of the data in bytes
 public record MerkleFooter(long chunkSize, long totalSize) {
 }
