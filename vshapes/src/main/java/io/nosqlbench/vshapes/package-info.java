60889f24d73ec05f63cf60db6809b37cde4961fe
Jonathan Shook <jshook@gmail.com>
2025-08-19 20:11:58 +0000

    stub package for statistics

---
+++ vshapes/src/main/java/io/nosqlbench/vshapes/package-info.java
@@ -0,0 +1,7 @@
+/// This package contains methods for understanding vector spaces.
+/// It should support the following:
+/// 1. Analysis of vector spaces to determine unique characteristics
+/// 2. Summarization and reporting on vector spaces
+/// 3. Extraction of descriptive statistical or other numerical metrics
+/// 4. Possible construction of similar vector spaces from extracted measures
+package io.nosqlbench.vshapes;
